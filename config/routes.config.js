09b7693c86517a190063f58dc02fa82c16090415
Marina Laporte Boni <marinaolboni@gmail.com>
2022-10-29 12:38:10 +0200

    payment WIP

--- config/routes.config.js
+++ config/routes.config.js
@@ -7,7 +7,7 @@ const authMiddleware = require("../middlewares/auth.middleware");
 const accountController = require('../controllers/account.controller');
 const messagesController = require('../controllers/messages.controller');
 const StripePackage = require('stripe');
-const stripe = StripePackage(process.env.SERVER_API_KEY);
+const stripe = StripePackage(process.env.STRIPE_API_KEY);
 
 
 const SCOPES = ["profile", "email"];
@@ -42,29 +42,29 @@ router.get("/account/fav/:property/:user", accountController.getOneFav);
 router.post("/account/favs", accountController.updateFav);
 
 //PAYMENT
-// router.post("/create-payment-intent", async (req, res) => {
-//     console.log('entra')
-//     const { items } = req.body;
+router.post("/create-payment-intent", async (req, res) => {
+    const { items } = req.body;
+    console.log('entra: ', items)
 
-//     const calculateOrderAmount = (items) => {
-//         // Replace this constant with a calculation of the order's amount
-//         // Calculate the order total on the server to prevent
-//         // people from directly manipulating the amount on the client
-//         return 1400;
-//       };
+    const calculateOrderAmount = (items) => {
+        // Replace this constant with a calculation of the order's amount
+        // Calculate the order total on the server to prevent
+        // people from directly manipulating the amount on the client
+        return 1400;
+      };
   
-//     // Create a PaymentIntent with the order amount and currency
-//     const paymentIntent = await stripe.paymentIntents.create({
-//       amount: calculateOrderAmount(items),
-//       currency: "usd",
-//       automatic_payment_methods: {
-//         enabled: true,
-//       },
-//     });
+    // Create a PaymentIntent with the order amount and currency
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: calculateOrderAmount(items),
+      currency: "usd",
+      automatic_payment_methods: {
+        enabled: true,
+      },
+    });
   
-//     res.send({
-//       clientSecret: paymentIntent.client_secret,
-//     });
-//   });
+    res.send({
+      clientSecret: paymentIntent.client_secret,
+    });
+  });
 
 module.exports = router;
